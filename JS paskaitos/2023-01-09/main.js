91c0a8fb36cf963b442605563a8bbcb8c34cfdf8
DarsStas <darstas@yahoo.com>
2023-01-11 21:51:28 +0200

    23-01-11 stopwatch 50% style

--- JS paskaitos/2023-01-09/main.js
+++ JS paskaitos/2023-01-09/main.js
@@ -1,26 +1,58 @@
-import timerState from "./state.js";
-import { formatTime } from "./utils.js";
+import TimerState from "./timerState.js";
+import { calculateTime, formatTime } from "./utils.js";
 
 const elements = {
   startButton: document.querySelector("#btn-start"),
   stopButton: document.querySelector("#btn-stop"),
+  resetButton: document.querySelector("#btn-reset"),
+  lapButton: document.querySelector("#btn-lap"),
   timerBox: document.querySelector("#timer"),
+  lapContainer: document.querySelector("#lap-container"),
+  lapParagraphElement: document.querySelector("p"),
+
 };
 
 function updateTimerText(secondsElapsed) {
-  const seconds = secondsElapsed % 60;
-  const minutes = Math.floor(secondsElapsed / 60) % 60;
-  const hours = Math.floor(Math.floor(secondsElapsed / 60) / 60);
+  const {seconds, minutes, hours} = calculateTime(secondsElapsed);
+  
 
   elements.timerBox.textContent = `${formatTime(hours)}:${formatTime(
     minutes
   )}:${formatTime(seconds)}`;
+};
+
+const createLap = (lapNumber, lapTime) => {
+  const lapParagraphElement = document.createElement("p");
+  const lapNumberNode = document.createTextNode(`Lap ${lapNumber}: `);
+  const lapTimeElement = document.createElement("span");
+  const {seconds, minutes, hours} = calculateTime(lapTime);
+  lapTimeElement.textContent = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
+  lapParagraphElement.append(lapNumberNode, lapTimeElement);
+  elements.lapContainer.append(lapParagraphElement);
+};
+
+const clearLaps = () => {
+  elements.lapContainer.innerHTML = "";
 }
 
-elements.startButton.addEventListener("click", function () {
-  timerState.start(updateTimerText);
+const timerState = new TimerState({
+  updateTime: updateTimerText,
+  setLap: createLap,
+  clearLaps: clearLaps,
 });
 
-elements.stopButton.addEventListener("click", function () {
+elements.startButton.addEventListener("click", () => {
+  timerState.start();
+});
+
+elements.stopButton.addEventListener("click", () => {
   timerState.stop();
+});
+
+elements.resetButton.addEventListener("click", () => {
+  timerState.reset();
+});
+
+elements.lapButton.addEventListener("click", () => {
+  timerState.lap();
 });
